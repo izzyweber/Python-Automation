51911b4cd6b090d38b38fe15009526c76bdae679
Izzy Weber <izzyweber@gmail.com>
2019-09-28 15:10:43 -0700

    define sheet and populate test 1 scores array

--- gradebook_automation.py
+++ gradebook_automation.py
@@ -1,2 +1,7 @@
 import openpyxl
-grades = openpyxl.load_workbook("samplegrades.xlsx")
+gradebook = openpyxl.load_workbook("sample_grades.xlsx")
+grades = gradebook.get_sheet_by_name("Sheet1")
+test_1_scores = []
+for i in range(2, grades.max_row + 1):
+  test_1_scores.append(grades["B" + str(i)].value)
+print(test_1_scores)
