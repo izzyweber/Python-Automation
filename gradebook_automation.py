0a57e51b8800eee0c322b0d16d944c01a8c0de52
Izzy Weber <izzyweber@gmail.com>
2019-09-28 15:14:31 -0700

    define all test score arrays

--- gradebook_automation.py
+++ gradebook_automation.py
@@ -1,7 +1,20 @@
 import openpyxl
 gradebook = openpyxl.load_workbook("sample_grades.xlsx")
+
+#define sheet
 grades = gradebook.get_sheet_by_name("Sheet1")
+
+#define test score arrays
 test_1_scores = []
 for i in range(2, grades.max_row + 1):
   test_1_scores.append(grades["B" + str(i)].value)
-print(test_1_scores)
+
+test_2_scores = []
+for i in range(2, grades.max_row + 1):
+  test_2_scores.append(grades["C" + str(i)].value)
+
+test_3_scores = []
+for i in range(2, grades.max_row + 1):
+  test_3_scores.append(grades["D" + str(i)].value)
+
+
